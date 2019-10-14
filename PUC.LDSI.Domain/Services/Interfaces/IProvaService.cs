196cff63fa7023d3aab10f1b6d9b18662906c826
Tiago <1085341@sga.pucminas.br>
2019-10-14 00:00:27 -0300

    ^commit jessica 3

---
+++ PUC.LDSI.Domain/Services/Interfaces/IProvaService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using PUC.LDSI.Domain.Entities;
+
+namespace PUC.LDSI.Domain.Services.Interfaces
+{
+    public interface IProvaService
+    {
+        Task<int> AdicionarProvaAsync(Avaliacao avaliacao, Aluno aluno, DateTime dataProva, decimal notaObtida);
+        Task<int> AlterarProvaAsync(int id, Avaliacao avaliacao, Aluno aluno, DateTime dataProva, decimal notaObtida);
+        Task ExcluirAsync(int id);
+    }
+}
